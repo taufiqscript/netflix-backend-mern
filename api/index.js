466b7f201aca88e568e33bf907ac64d38cea4d9a
unknown <28.taufiqurrahman@gmail.com>
2025-06-21 15:44:53 +0700

    update for vercel serverless

--- api/index.js
+++ api/index.js
@@ -2,22 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const router = require('./routes/index.route')
+const router = require('../routes/index.route')
 const { MONGODB_URL } = process.env
 const swaggerUI = require('swagger-ui-express')
 const YAML = require('yamljs')
-const swaggerDocs = YAML.load('./swagger.yaml')
+const serverless = require('serverless-http')
+const path = require('path')
 
 const app = express()
 
 app.use(cors({ origin: true, credentials: true }))
 app.use(express.json())
 
-app.use(
-    "/docs",
-    swaggerUI.serve,
-    swaggerUI.setup(swaggerDocs)
-)
+// Swagger setup (optional in prod)
+const swaggerPath = path.join(__dirname, '..', 'swagger.yaml');
+const swaggerDocs = YAML.load(swaggerPath);
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 mongoose.connect(MONGODB_URL).catch(err => {
     if (err) {
@@ -30,14 +30,9 @@ mongoose.connect(MONGODB_URL).catch(err => {
 
 app.use('/api', router)
 
-const PORT = process.env.PORT || 3002
-
 app.get('/', (req, res) => {
     res.send('API Netflix Clone is running...')
 })
 
-app.listen(PORT, () => {
-    console.log("server berjalan di port " + PORT)
-})
-
 module.exports = app
+module.exports.handler = serverless(app)
